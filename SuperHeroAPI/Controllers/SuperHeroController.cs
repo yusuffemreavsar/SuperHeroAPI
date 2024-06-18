417b9e72c94dff89b5c7f9efac168866611dfdea
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-06-18 23:09:27 +0300

    Controller & Data Layer Created.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Entities;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+
+    }
+}
